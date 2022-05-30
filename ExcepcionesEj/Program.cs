3d79025484457134d0f2b82ca86b79f5ea754497
AAngel777 <105814810+AAngel777@users.noreply.github.com>
2022-05-30 10:16:04 -0700

    Ejmplo excepciones

---
+++ ExcepcionesEj/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcepcionesEj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+                Console.ReadLine();
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            catch (FormatException a)
+            {
+                Console.WriteLine(a.Message);
+                Console.WriteLine(a.StackTrace);
+            }
+            Console.ReadKey();
+
+        }
+    }
+    }
